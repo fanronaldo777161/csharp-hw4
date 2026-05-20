a04b737f8605e61e3b4e8d11f3a2ed4fe2ef8074
fanronaldo777161 <siharylidze091007@gmail.com>
2026-05-20 16:21:03 +0300

    task1

---
+++ homework4/homework4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace homework4
+{
+    internal class Program
+    {
+        //task1
+        static bool task1(string s)
+        {
+            string p = @"\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b";
+            return Regex.IsMatch(s, p);
+        }
+        static void Main(string[] args)
+        {
+            //task1
+            Console.WriteLine(task1("192.192.192.192"));
+            Console.WriteLine(task1("255.255.255.255"));
+            Console.WriteLine(task1("999.1.1.1")); // false
+        }
+    }
+}
